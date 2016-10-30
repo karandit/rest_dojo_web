ac15f0409d7b1a7a0f0874e5661c45a0fad887ae
Karandi Tamas <tkarandi@gmail.com>
2016-10-30 22:37:45 +0100

    Display cards

--- src/RestDojo/Cluedo/CluedoView.elm
+++ src/RestDojo/Cluedo/CluedoView.elm
@@ -1,14 +1,13 @@
 module RestDojo.Cluedo.CluedoView exposing (view)
 
-import Html exposing (Html, text, div, hr)
+import Html exposing (Html, text, div, hr, img)
+import Html.Attributes exposing (title, src, width, height)
 import RestDojo.Types exposing (..)
 
 
 view : Game -> List (Html Msg)
 view game =
-    [ div [] [ text <| toString game.secret.person ]
-    , div [] [ text <| toString game.secret.weapon ]
-    , div [] [ text <| toString game.secret.location ]
+    [ div [] <| List.map viewCard [ toString game.secret.person, toString game.secret.weapon, toString game.secret.location ]
     , hr [] []
     ]
         ++ (List.map viewBot game.bots)
@@ -16,14 +15,23 @@ view game =
 
 viewBot : Bot -> Html Msg
 viewBot bot =
-    div []
-        [ div [] <| List.map viewCard bot.persons
-        , div [] <| List.map viewCard bot.locations
-        , div [] <| List.map viewCard bot.weapons
-        , hr [] []
-        ]
+    let
+        allBotCards =
+            (List.map toString bot.persons) ++ (List.map toString bot.locations) ++ (List.map toString bot.weapons)
+    in
+        div [] <| List.map viewCardSmall allBotCards
 
 
-viewCard : a -> Html Msg
-viewCard card =
-    text <| toString card
+viewCard : String -> Html Msg
+viewCard =
+    viewCardWithSize 144 180
+
+
+viewCardSmall : String -> Html Msg
+viewCardSmall =
+    viewCardWithSize 80 100
+
+
+viewCardWithSize : Int -> Int -> String -> Html Msg
+viewCardWithSize w h cardName =
+    img [ src <| "img/cards/" ++ cardName ++ ".png", width w, height h, title cardName ] []
