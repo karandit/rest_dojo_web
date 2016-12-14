33b3ef1dde22c23ce9888f56367072da3c474400
Karandi Tamas <tkarandi@gmail.com>
2016-12-14 14:17:36 +0100

    Add the dojoId to the game url

--- src/RestDojo/ViewDojo.elm
+++ src/RestDojo/ViewDojo.elm
@@ -259,7 +259,7 @@ viewEventGameWonBy dojo gameUrl team =
     in
         div [ class "rd-team" ]
             [ span [ class "rd-team-name" ]
-                [ a [ href "#", onClick <| SelectGame dojo gameUrl ] [ text "Game" ]
+                [ a [ href <| "#dojos/" ++ (toString dojo.id) ++ "/games/999", onClick <| SelectGame dojo gameUrl ] [ text "Game" ]
                 , text label
                 ]
             , img avatarAttr []
